e391e4ba8db9c5533ee6f7a61e46fd9dfdb6ed4b
semenko-vladimir <volodya2000lichnoe@gmail.com>
2024-09-22 16:01:51 +0300

    Add 2 new buttons: To get the general state of the market by tickers at the moment and to get market ups by tickers at the moment

--- bot.py
+++ bot.py
@@ -93,6 +93,8 @@ def start(message):
         ticker_delete_button = types.KeyboardButton('Удалить тикер')
         tickers_delete_all_button = types.KeyboardButton('Удалить мои тикеры')
         receive_market_collapse_button = types.KeyboardButton('Получить обвал рынка по тикерам')
+        receive_market_update_button = types.KeyboardButton('Получить рост рынка по тикерам')
+        all_market_status_button = types.KeyboardButton('Получить изменение состояния рынка по тикерам')
         subscribe_to_collapse_update_button = types.KeyboardButton('Подписаться на обновления падений рынка')
         unsubscribe_to_collapse_update_button = types.KeyboardButton('Отписаться от обновления падений рынка')
         subscribe_to_market_update_button = types.KeyboardButton('Подписаться на обновления рынка')
@@ -105,8 +107,9 @@ def start(message):
 
         keyboard.row(subscribe_to_collapse_update_button, unsubscribe_to_collapse_update_button)
 
-        keyboard.row(subscribe_to_market_update_button, unsubscribe_to_market_update_button)
+        keyboard.row(subscribe_to_market_update_button, unsubscribe_to_market_update_button, receive_market_update_button)
 
+        keyboard.row(all_market_status_button)
         
         bot.send_message(message.chat.id, 'Добро пожаловать!', reply_markup=keyboard)
 
@@ -303,6 +306,176 @@ def percent_handler(call):
             bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
         elif percent == 'более 20%' and price_change_percent <= -20:
             bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+
+
+
+# <==============================================================================================================================>
+
+# Обработчик для получения роста рынка по тикерам
+@bot.message_handler(func=lambda message: message.text == 'Получить рост рынка по тикерам')
+# TODO: Refactor this function
+def add_ticker_handler(message):
+    chat_id = message.chat.id
+    token = get_t_token(chat_id)
+    if token is not None:
+        tickers = get_all_tickers(chat_id)
+
+        if not tickers:
+            bot.send_message(chat_id, 'У вас нет активных тикеров')
+        else:
+            inline_keyboard = types.InlineKeyboardMarkup()
+            buttons = [
+                types.InlineKeyboardButton(text='10 минут', callback_data='intervalu_10 минут'),
+                types.InlineKeyboardButton(text='час', callback_data='intervalu_час'),
+                types.InlineKeyboardButton(text='день', callback_data='intervalu_день'),
+                types.InlineKeyboardButton(text='неделя', callback_data='intervalu_неделя'),
+                types.InlineKeyboardButton(text='месяц', callback_data='intervalu_месяц'),
+                types.InlineKeyboardButton(text='год', callback_data='intervalu_год')
+            ]
+            inline_keyboard.add(*buttons)
+            bot.send_message(chat_id, 'Выберите интервал', reply_markup=inline_keyboard)
+
+# Обработчик для callback выбора интервала
+@bot.callback_query_handler(func=lambda call: call.data.startswith('intervalu_'))
+def interval_handler(call):
+    interval = call.data.replace('intervalu_', '')
+    inline_keyboard = types.InlineKeyboardMarkup()
+    buttons = [
+        types.InlineKeyboardButton(text='до 2%', callback_data=f'percentu_до 2%_{interval}'),
+        types.InlineKeyboardButton(text='от 2% до 5%', callback_data=f'percentu_от 2% до 5%_{interval}'),
+        types.InlineKeyboardButton(text='от 5% до 10%', callback_data=f'percentu_от 5% до 10%_{interval}'),
+        types.InlineKeyboardButton(text='от 10% до 20%', callback_data=f'percentu_от 10% до 20%_{interval}'),
+        types.InlineKeyboardButton(text='более 20%', callback_data=f'percentu_более 20%_{interval}'),
+        types.InlineKeyboardButton(text='Общее состояние', callback_data=f'percentu_до 100%_{interval}'),
+    ]
+    inline_keyboard.add(*buttons)
+    bot.send_message(call.message.chat.id, 'Выберите процент', reply_markup=inline_keyboard)
+
+# Обработчик для callback выбора процента
+@bot.callback_query_handler(func=lambda call: call.data.startswith('percentu_'))
+# TODO: Refactor this function
+def percent_handler(call):
+    data = call.data.split('_')
+    percent = data[1]
+    interval = data[2]
+
+    # Логика для получения данных о тикерах
+    chat_id = call.message.chat.id
+    tickers = get_all_tickers(chat_id)
+    
+    for ticker in tickers:
+        info = get_info_by_ticker(str(ticker[0]))
+        figi = info['figi'].values[0:1][0]
+        name = info['name'].values[0:1][0]
+        type_of = info['type'].values[0:1][0]
+
+        if interval == '10 минут':
+            start_time = datetime.now() - timedelta(minutes=10)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_1_MIN
+        elif interval == 'час':
+            start_time = datetime.now() - timedelta(hours=1)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_1_MIN
+        elif interval == 'день':
+            start_time = datetime.now().replace(hour=10, minute=0, second=0)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_10_MIN
+        elif interval == 'неделя':
+            start_time = datetime.now() - timedelta(weeks=1)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_DAY
+        elif interval == 'месяц':
+            start_time = datetime.now() - timedelta(days=30)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_WEEK
+        elif interval == 'год':
+            start_time = datetime.now() - timedelta(days=365)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_MONTH
+
+        end_time = datetime.now()
+        price_change, price_change_percent, max_price, min_price, close_price = get_price_change_in_current_interval(figi, start_time, end_time, candle_interval)
+
+        # Проверка процента изменения цены
+        if percent == 'до 100%' and price_change_percent > 0.01:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        if percent == 'до 2%' and 0 < price_change_percent < 2:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        elif percent == 'от 2% до 5%' and 2 <= price_change_percent <= 5:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        elif percent == 'от 5% до 10%' and 5 <= price_change_percent <= 10:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        elif percent == 'от 10% до 20%' and 10 <= price_change_percent <= 20:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        elif percent == 'более 20%' and price_change_percent >= 20:
+            bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+
+
+# <==============================================================================================================================>
+
+# Обработчик для получения изменеия состояния рынка по тикерам
+@bot.message_handler(func=lambda message: message.text == 'Получить изменение состояния рынка по тикерам')
+# TODO: Refactor this function
+def add_ticker_handler(message):
+    chat_id = message.chat.id
+    token = get_t_token(chat_id)
+    if token is not None:
+        tickers = get_all_tickers(chat_id)
+
+        if not tickers:
+            bot.send_message(chat_id, 'У вас нет активных тикеров')
+        else:
+            inline_keyboard = types.InlineKeyboardMarkup()
+            buttons = [
+                types.InlineKeyboardButton(text='10 минут', callback_data='intervals_10 минут'),
+                types.InlineKeyboardButton(text='час', callback_data='intervals_час'),
+                types.InlineKeyboardButton(text='день', callback_data='intervals_день'),
+                types.InlineKeyboardButton(text='неделя', callback_data='intervals_неделя'),
+                types.InlineKeyboardButton(text='месяц', callback_data='intervals_месяц'),
+                types.InlineKeyboardButton(text='год', callback_data='intervals_год')
+            ]
+            inline_keyboard.add(*buttons)
+            bot.send_message(chat_id, 'Выберите интервал', reply_markup=inline_keyboard)
+
+
+# Обработчик для callback выбора процента
+@bot.callback_query_handler(func=lambda call: call.data.startswith('intervals_'))
+# TODO: Refactor this function
+def percent_handler(call):
+    data = call.data.split('_')
+    interval = data[1]
+
+    # Логика для получения данных о тикерах
+    chat_id = call.message.chat.id
+    tickers = get_all_tickers(chat_id)
+    
+    for ticker in tickers:
+        info = get_info_by_ticker(str(ticker[0]))
+        figi = info['figi'].values[0:1][0]
+        name = info['name'].values[0:1][0]
+        type_of = info['type'].values[0:1][0]
+
+        if interval == '10 минут':
+            start_time = datetime.now() - timedelta(minutes=10)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_1_MIN
+        elif interval == 'час':
+            start_time = datetime.now() - timedelta(hours=1)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_1_MIN
+        elif interval == 'день':
+            start_time = datetime.now().replace(hour=10, minute=0, second=0)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_10_MIN
+        elif interval == 'неделя':
+            start_time = datetime.now() - timedelta(weeks=1)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_DAY
+        elif interval == 'месяц':
+            start_time = datetime.now() - timedelta(days=30)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_WEEK
+        elif interval == 'год':
+            start_time = datetime.now() - timedelta(days=365)
+            candle_interval = CandleInterval.CANDLE_INTERVAL_MONTH
+
+        end_time = datetime.now()
+        price_change, price_change_percent, max_price, min_price, close_price = get_price_change_in_current_interval(figi, start_time, end_time, candle_interval)
+
+        bot.send_message(chat_id, f'Название: {name}\n Тип: {type_of}\n Тикер: {ticker}\n Изменение цены: {round(price_change_percent, 2)}% \n Цена закрытия последней свечи: {close_price} \n Максимальная цена: {max_price} \n Минимальная цена: {min_price}')
+        
+
+
 
 chat_schedulers = {}
 
