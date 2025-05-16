faf0357aa4e5fc4dd4870373f40001668f2de43e
semenko-vladimir <volodya2000lichnoe@gmail.com>
2025-05-16 11:56:06 +0300

    fix: token problem

--- app/client/bot/bot.py
+++ app/client/bot/bot.py
@@ -1,9 +1,13 @@
 import telebot
-
 from dotenv import load_dotenv
 import os
+import codecs
 
 # Создание объекта бота
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+if BOT_TOKEN:
+    BOT_TOKEN = codecs.decode(BOT_TOKEN, 'unicode_escape')
+
 bot = telebot.TeleBot(BOT_TOKEN)
